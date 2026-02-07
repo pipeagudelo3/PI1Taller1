eb341839f11dd362e993f19db929de46830ae217
pipeagudelo3 <125285931+pipeagudelo3@users.noreply.github.com>
2026-02-07 10:28:13 -0500

    modelo Movie, admin, media y home con busqueda

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to="movie/images/")
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
